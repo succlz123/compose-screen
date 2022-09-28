succlz123/compose-desktop-imageloader

feat: init commit

desktop/build.gradle.kts
@@ -1,0 +1,41 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+group = "io.github.succlz123"
+version = "0.0.1"
+
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+        withJava()
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(project(":compose-imageloader"))
+                implementation(compose.desktop.currentOs)
+            }
+        }
+        val jvmTest by getting
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "compose-desktop-imageloader"
+            packageVersion = "1.0.0"
+        }
+    }
+}

succlz123/compose-screen

desktop/build.gradle.kts
@@ -1,0 +1,40 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+group = "io.github.succlz123"
+version = "0.0.1"
+
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+        withJava()
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(project(":compose-screen"))
+                implementation(compose.desktop.currentOs)
+            }
+        }
+        val jvmTest by getting
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "compose-screen-desktop"
+            packageVersion = "1.0.0"
+        }
+    }
+}